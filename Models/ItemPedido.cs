8d9f0c76d3579bb09c66bacfa4fa1c93a1f5e829
Alexandre Angeli de Lima <alexandrelima@Alexandres-MacBook-Air.local>
2020-04-03 00:12:13 -0300

    sdk core back to 2.2 :(

--- Models/ItemPedido.cs
+++ Models/ItemPedido.cs
@@ -1,8 +1,7 @@
-﻿using System;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Runtime.Serialization;
-using System.Text.Json.Serialization;
+using Newtonsoft.Json;
 
 namespace ECommerceLiteAlexandre.Models
 {
--- Startup.cs
+++ Startup.cs
@@ -30,7 +30,7 @@ namespace ECommerceLiteAlexandre
             });
 
 
-            services.AddMvc(option => option.EnableEndpointRouting = false).SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
+            services.AddMvc(option => option.EnableEndpointRouting = false).SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
 
             services.AddHttpContextAccessor();
 
@@ -50,7 +50,7 @@ namespace ECommerceLiteAlexandre
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, 
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, 
             IServiceProvider serviceProvider)
         {
             if (env.EnvironmentName == "Development")
